be51a14276af68bd54b9f17e055880d13b52ecda
srii03 <162790549+srii03@users.noreply.github.com>
2024-03-29 15:36:58 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var outputParagraph = document.getElementById("output");
+    outputParagraph.textContent = "This text is updated by JavaScript.";
+});
