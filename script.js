9c73bafbf807fe3fff5391c139140c9cf853b60f
srii03 <162790549+srii03@users.noreply.github.com>
2024-04-17 19:31:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var outputParagraph = document.getElementById("output");
-    outputParagraph.textContent = "This text is updated by JavaScript.";
-});
+function changeText() {
+    let potentialText = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let randomNumber = getRandomNumber(0, potentialText.length - 1);
+    let textToChangeTo = potentialText[randomNumber];
+    document.getElementById('heading').innerHTML = textToChangeTo;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
